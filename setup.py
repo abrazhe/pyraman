4780607ca8d8646237cc30c2bfd2a6bcf56abf71
Alexey Brazhe <brazhe@gmail.com>
2009-11-21 16:16:16 +0300

    Changed version to 0.2.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ classifiers=[
 
 
 setup(name='pyraman',
-      version='0.2',
+      version='0.2.1',
       description = 'Lib to process Raman spectra in pylab',
       author = 'Alexey Brazhe',
       py_modules = ['raman'],
