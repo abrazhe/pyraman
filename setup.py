f96511b1f67a7fad0b1c4bcbefb60340c838b064
Alexey Brazhe <brazhe@gmail.com>
2009-11-21 13:10:35 +0300

    added classifiers

--- setup.py
+++ setup.py
@@ -2,6 +2,17 @@
 
 from distutils.core import setup
 
+classifiers=[
+      'Development Status :: 4 - Beta',
+      'Environment :: X11 Applications',
+      'Intended Audience :: End Users/Desktop',
+      'License :: OSI Approved :: GNU General Public License (GPL)',
+      'Operating System :: POSIX :: Linux',
+      'Programming Language :: Python',
+      'Topic :: Scientific/Engineering',
+        ]
+
+
 setup(name='pyraman',
       version='0.2',
       py_modules = ['raman'],
