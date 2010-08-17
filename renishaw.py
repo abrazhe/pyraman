747647b93246209842d521c75bdc0a1214fc94e8
Alexey Brazhe <brazhe@gmail.com>
2010-08-17 19:45:51 +0200

    added range_rms function, more features in inspect_images

--- renishaw.py
+++ renishaw.py
@@ -1,6 +1,7 @@
 ## Small library for reading images and spectra exported as txt from Renishaw software
 
 import numpy as np
+import pylab as pl
 
 def load_image(fname):
     "Loads image exported as text from Renishaw software"
@@ -22,20 +23,19 @@ def process_image(arr, fn):
     "Apply a function to each spectrum in an image"
     figsh = arr.shape[:2]
     new_shape= figsh + fn(arr[0,0]).shape
-    out = zeros(new_shape)
+    out = np.zeros(new_shape)
     nrow,ncol = figsh
     for r in xrange(nrow):
         for c in xrange(ncol):
             out[r,c] = fn(arr[r,c])
     return out
 
-def inspect_image(arr, nu):
+def inspect_image(arr, nu, fn = range_rms):
     "Inspect an image: show spectra for a pixel mouse hovers on"
-    import pylab as pl
     fh  = pl.figure();
     ax2 = pl.subplot(122);
     plh = pl.plot(nu, arr[0,0], 'k-')[0]
-    axis((nu[0], nu[-1], arr.min(), arr.max()))
+    pl.axis((nu[0], nu[-1], arr.min(), arr.max()))
 
     def _on_hover(event):
         if event.inaxes == ax1:
@@ -45,7 +45,7 @@ def inspect_image(arr, nu):
             fh.canvas.draw()
             pass
     ax1 = pl.subplot(121);
-    pl.imshow(range_sum(arr, nu, (nu.min(), nu.max())),
+    pl.imshow(fn(arr, nu, (nu.min(), nu.max())),
               aspect='equal', interpolation='nearest')
     fh.canvas.mpl_connect('motion_notify_event', _on_hover)
 
@@ -55,7 +55,13 @@ def in_range(vec, range):
 def range_sum(arr, nu, nurange):
     "Sum intensities in the given nu range"
     return process_image(arr, lambda x: np.sum(x[in_range(nu,nurange)]))
+
+def range_rms(arr, nu, nurange):
+    "RMS intensities in the given nu range"
+    return process_image(arr, lambda x: pl.rms_flat(x[in_range(nu,nurange)]))
     
+
+
 def load_spectrum(fname):
     "Read spectrum exported as text from Renishaw software"
     spec = np.loadtxt(fname)
